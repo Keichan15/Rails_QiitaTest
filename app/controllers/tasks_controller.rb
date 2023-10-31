6d78d7210b08ba352a21c50d20db78ca3820316b
keichan15 <keisuke1515gaming@outlook.jp>
2023-10-31 22:36:58 +0900

    Initial Commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,18 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :body)
+  end
+end
